a2a3e4fafdce1fbc4eca61023296ff6bad784268
User <tjasur@gmail.com>
2024-01-15 17:40:38 +0500

    15.01

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import *
+
 
 urlpatterns = [
-    path('', home_page, name='home_page'),  # hech narsa bosilmasa, 'home_page'ga o'tilsin
-
+    path('', home_page, name='home_page'),                   # hech narsa bosilmasa, 'home_page'ga o'tilsin
+    path('login_page/', login_page, name='login_page'),
+    path('logout_page/', logout_page, name='logout_page'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from adminapp import services
+
+
+def login_required_decorator(func):
+    return login_required(func, login_url='login_page')
+
+
+@ login_required_decorator
+def home_page(request):
+    faculties = services.get_faculties()
+    kafedras = services.get_kafedra()
+    return render(request, 'index.html')
