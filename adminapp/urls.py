671ec202470c8a39cf4eb9ccb17a29355ed3c27b
User <tjasur@gmail.com>
2024-01-16 10:59:11 +0500

    16.01

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', home_page, name='home_page'),                   # hech narsa bosilmasa, 'home_page'ga o'tilsin
     path('login_page/', login_page, name='login_page'),
     path('logout_page/', logout_page, name='logout_page'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
 from adminapp import services
 
@@ -8,8 +12,29 @@ def login_required_decorator(func):
     return login_required(func, login_url='login_page')
 
 
+def login_page(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, password=password, username=username)
+        if user is not None:
+            login(request, user)
+            return redirect('home_page')
+
+    return render(request, 'login.html')
+
+
+@login_required_decorator
+def logout_page(func):
+    return login_required(func, login_url='login_page')
+
+
 @ login_required_decorator
 def home_page(request):
-    faculties = services.get_faculties()
-    kafedras = services.get_kafedra()
     return render(request, 'index.html')
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login_page')
+    template_name = 'signup_html'
